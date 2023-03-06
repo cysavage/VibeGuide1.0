61723e025a7eca8de53a77d7509b57cbfba3b08e
cysavage <cyrilkthomasest.1991@gmail.com>
2023-03-05 20:22:52 -0500

    fix

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
-<?php 
+<?php
 include_once 'header.php';
 ?>
-        <div class="banner-area">
-            <h2>VIBE GUIDES</h2>
-		</a>
-        </div>
-        <div class="content-area">
-            <div class="wrapper">
-                <h2>Discover Your Vibe</h2>
+<div class="banner-area">
+    <h2>VIBE GUIDES</h2>
+    </a>
+</div>
+<div class="content-area">
+    <div class="wrapper">
+        <h2>Discover Your Vibe</h2>
 
-               <div id="newmap" class="map" style="height:1000px; width:1400"></div>
-               
-                           </div>
-        </div>
+        <div id="newmap" class="map" style="height:1000px; width:1400"></div>
+
+    </div>
+</div>
 
 </body>
+
 </html>
 
 
-<?php 
+<?php
 include_once 'footer.php';
 ?>
